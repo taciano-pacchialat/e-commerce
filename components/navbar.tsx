0b0cb42cbae79b319c27e1ce4b1e387bac4f9a48
Taciano <taciano3003@gmail.com>
2023-10-03 17:22:34 -0300

    initial tweaks and navbar placeholders

---
+++ components/navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="position: fixed top -0 flex flex-row items-center bg-lightbrown w-screen h-16 shadow-lg">
+      <i>1</i>
+      <i>2</i>
+      <i>3</i>
+      <i>4</i>
+      <i>5</i>
+    </nav>
+  );
+};
+
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,21 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
+  mode: "jit",
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  purge: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        lightbrown: "#C4A605",
+        darkbrown: "765827",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
